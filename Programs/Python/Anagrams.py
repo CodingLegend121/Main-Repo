07536fd6df9b816894be6106cc2011ea1e9dfa4d
Charan <charan.adhish@gmail.com>
2018-03-18 13:21:09 -0400

    Fixed lines problem
    It used to think the word is not in lines

--- Programs/Python/Anagrams.py
+++ Programs/Python/Anagrams.py
@@ -1,7 +1,21 @@
-import itertools
+#import itertools
+#from termcolor import colored
 
-word = str(input('Word:'))
-word = list(word)
+
+word ='what'
+#'''input('Word:').lower()'''
+#word = list(str(word))
+'''
 anagrams = ["".join(perm) for perm in itertools.permutations(word)]
-anagrams = '\n'.join(map(str,anagrams))
-print(anagrams)
+anagrams = '\n'.join(map(str,anagrams))'''
+file = open('wordlist.txt', 'r')
+lines = file.read().split('\n')
+'''if anagrams[] in lines:
+	print(colored(anagrams,'green'))
+else:
+	print(colored(anagrams,'red'))
+	'''
+if word in lines:
+	print('Noice')
+else:
+	print('You suck')
