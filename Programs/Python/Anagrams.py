159f31532bfd1612abd5624a985af6e3e7105b06
Charan <charan.adhish@gmail.com>
2018-03-18 15:32:15 -0400

    Almost done

--- Programs/Python/Anagrams.py
+++ Programs/Python/Anagrams.py
@@ -1,21 +1,14 @@
-#import itertools
-#from termcolor import colored
+import itertools
+from time import sleep
 
-
-word ='what'
-#'''input('Word:').lower()'''
-#word = list(str(word))
-'''
+word = input('Word:').lower()
+word = list(str(word))
 anagrams = ["".join(perm) for perm in itertools.permutations(word)]
-anagrams = '\n'.join(map(str,anagrams))'''
 file = open('wordlist.txt', 'r')
 lines = file.read().split('\n')
-'''if anagrams[] in lines:
-	print(colored(anagrams,'green'))
-else:
-	print(colored(anagrams,'red'))
-	'''
-if word in lines:
-	print('Noice')
-else:
-	print('You suck')
+for n, i in enumerate(anagrams):
+			if i not in lines:
+    				anagrams[n] = ''
+anagrams = '\n'.join(map(str,anagrams))
+print(anagrams.strip(''))
+sleep(100)
