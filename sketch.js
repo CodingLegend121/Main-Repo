9c39c681ac660237802c4098543bbedfea1d5858
Charan <charan.adhish@gmail.com>
2018-08-21 14:16:18 -0400

    More Box Shadows

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "13vw";
+  document.getElementById("main").style.marginLeft = "12vw";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
+if (window.innerWidth < 650) {
+  console.log('Hooray!');
+}
