88abae21502eec99d47709e6c1b21dbeed2cfb82
Charan <charan.adhish@gmail.com>
2018-08-21 19:32:48 -0400

    Updated local p5 lib to cloud

--- Mini-Programs/P5/Clock/FirstP5.js
+++ Mini-Programs/P5/Clock/FirstP5.js
@@ -1,11 +1,12 @@
 function setup() {
-  createCanvas(600,600,)
+  createCanvas(600, 600, )
   angleMode(DEGREES)
 
 }
+
 function draw() {
   background(255)
-  translate(300,300)
+  translate(300, 300)
   rotate(-90)
 
   var s = second()
@@ -14,18 +15,18 @@ function draw() {
   var mi = millis()
 
   noFill()
-  arc(0,0,390,390,0,s*6)
-  stroke(100,255,200)
+  arc(0, 0, 390, 390, 0, s * 6)
+  stroke(100, 255, 200)
   strokeWeight(30)
 
   noFill()
-  arc(0,0,320,320,0,m*6)
-  stroke(255,100,100)
+  arc(0, 0, 320, 320, 0, m * 6)
+  stroke(255, 100, 100)
   strokeWeight(30)
 
   noFill()
-  arc(0,0,250,250,0,h*30)
-  stroke(100,200,255)
+  arc(0, 0, 250, 250, 0, h * 12)
+  stroke(100, 200, 255)
   strokeWeight(30)
 
 
